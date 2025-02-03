64fda7dea17ff5771674da18030ae9534c7cf724
RahulRouchanGogoi <119352012+RahulRouchanGogoi@users.noreply.github.com>
2025-02-03 17:24:50 +0530

    Add files via upload

---
+++ 02-Basics/_04variables.java
@@ -0,0 +1,18 @@
+public class _04variables 
+{
+    public static void main(String[] args)
+    {
+        int a=15;
+        float b=4.15f;
+        char c='A';
+        String name="Rahul Rouchan Gogoi";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(name);
+
+        
+    }
+    
+}
